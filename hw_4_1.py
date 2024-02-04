7c7753a1aa0838831432d1dcc91cefc2403b24af
victorcoffee <106419325+victorcoffee@users.noreply.github.com>
2024-02-04 19:06:00 +0200

    Module-4-version-0.3

--- hw_4_1.py
+++ hw_4_1.py
@@ -2,12 +2,14 @@
 import os
 from pathlib import Path
 
+# Функція підраховує сумарну і середню зарплату за даними зі вказаного файла
 
 def total_salary(path):
     path_file = Path(path)
     if path_file.exists():
         with open(path, "r", encoding="utf-8") as f:
             lines = f.readlines()
+
         total = 0
         count = 0
         for line in lines:
@@ -15,10 +17,12 @@ def total_salary(path):
             salary = float(salary)
             total += salary
             count += 1
+
         if count > 0:
             average = total / count
         else:
             average = 0
+
         return (total, average)
     else:
         print(f"Файл {path_file} не існує")
